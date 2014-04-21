64f068cafcbab6687e7bc1173bb9d5040ce2c5c2
Felix <mail@nicidienase.de>
2014-04-21 21:34:32 +0200

    added Data-Model

---
+++ pushclient/src/main/java/de/nicidienase/push/pushclient/Model/Group.java
@@ -0,0 +1,14 @@
+package de.nicidienase.push.pushclient.Model;
+
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by felix on 21.04.14.
+ */
+@Table(name = "groups")
+public class Group {
+
+    @Column
+    public String groupname;
+}
---
+++ pushclient/src/main/java/de/nicidienase/push/pushclient/Model/Notification.java
@@ -0,0 +1,27 @@
+package de.nicidienase.push.pushclient.Model;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+import java.util.Date;
+
+/**
+ * Created by felix on 21.04.14.
+ */
+@Table(name = "notifications")
+public class Notification extends Model {
+
+    @Column
+    public String title;
+    @Column
+    public String message;
+    @Column
+    public String long_message;
+    @Column
+    public Date received;
+    @Column
+    public int priority;
+    @Column
+    public Group group;
+}
