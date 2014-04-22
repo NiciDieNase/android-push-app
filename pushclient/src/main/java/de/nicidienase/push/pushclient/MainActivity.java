72d0fcf1eaf40e4d5ebbd827deccd164a4e856e6
Felix <mail@nicidienase.de>
2014-04-22 13:42:05 +0200

    adds menu-item to generate items for testing and housekeeping

--- pushclient/src/main/java/de/nicidienase/push/pushclient/MainActivity.java
+++ pushclient/src/main/java/de/nicidienase/push/pushclient/MainActivity.java
@@ -1,33 +1,37 @@
 package de.nicidienase.push.pushclient;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Date;
+
+import de.nicidienase.push.pushclient.Model.Notification;
+
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
 
-        ListFragment listFragment = new ListFragment();
-        listFragment.setListAdapter(new NotificationAdapter(this));
-        getFragmentManager().beginTransaction()
-                .replace(R.id.fragmentcontainer, listFragment)
-                .commit();
-    }
+		ListFragment listFragment = new ListFragment();
 
-    @Override
+		NotificationAdapter notificationAdapter = new NotificationAdapter(this);
+
+		listFragment.setListAdapter(notificationAdapter);
+		getFragmentManager().beginTransaction()
+				.replace(R.id.fragmentcontainer, listFragment)
+				.commit();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.controller, menu);
@@ -41,6 +45,14 @@ public class MainActivity extends Activity {
 			Intent j = new Intent(getApplicationContext(), SettingsActivity.class);
 			startActivity(j);
 			break;
+		case R.id.action_add_items:
+			for(int i = 0; i<10;i++){
+				Notification n = new Notification();
+				n.title = "Title " + i;
+				n.message = "Message " + i;
+				n.received = new Date();
+				n.save();
+			}
 
 		default:
 			break;
