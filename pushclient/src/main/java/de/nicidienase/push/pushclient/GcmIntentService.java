16d555efe9c40ad0fc1fab0e56fa18dc3c1f7206
Felix <mail@nicidienase.de>
2014-04-27 23:50:01 +0200

    adds intent to notifications

--- pushclient/src/main/java/de/nicidienase/push/pushclient/GcmIntentService.java
+++ pushclient/src/main/java/de/nicidienase/push/pushclient/GcmIntentService.java
@@ -3,6 +3,8 @@ package de.nicidienase.push.pushclient;
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -53,12 +55,18 @@ public class GcmIntentService extends IntentService {
 					boolean sound = preferences.getBoolean(getString(R.string.setting_key_sound),false);
 					boolean vibrate = preferences.getBoolean(getString(R.string.setting_key_vibrate),false);
 					if(notify){
+						Intent i = new Intent(this, MainActivity.class);
+						TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+						stackBuilder.addNextIntent(i);
+						PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+
 						Notification.Builder mBuilder = new Notification.Builder(this)
 								.setSmallIcon(android.R.drawable.ic_dialog_email)
 								.setContentTitle(title)
 								.setContentText(msg)
-								.setAutoCancel(true);
-						Notification noti = mBuilder.getNotification();
+								.setAutoCancel(true)
+								.setContentIntent(pendingIntent);
+						Notification noti = mBuilder.build();
 						if(sound){
 							noti.defaults |= Notification.DEFAULT_SOUND;
 						}
