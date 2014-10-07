3dd7cb82854c88fa35602d62abef509fa2d36163
Felix Bürkle <f.buerkle@inovex.de>
2014-10-07 16:13:50 +0200

    adds Wear-Features to Notifications

--- pushclient/src/main/java/de/nicidienase/push/pushclient/GcmIntentService.java
+++ pushclient/src/main/java/de/nicidienase/push/pushclient/GcmIntentService.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -19,7 +20,8 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
  * Created by felix on 27.04.14.
  */
 public class GcmIntentService extends IntentService {
-	private static final int MSG_LENGTH = 80;
+	private static final int MSG_LENGTH = 50;
+	public static final int NOTIFICATION_ID = 2342;
 
 	/**
 	 * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -34,8 +36,6 @@ public class GcmIntentService extends IntentService {
 		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
 
 		String messageType = gcm.getMessageType(intent);
-
-		int notificationID = 2342;
 
 		if (!extras.isEmpty()) {
 			if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
@@ -72,28 +72,60 @@ public class GcmIntentService extends IntentService {
 					boolean notify = preferences.getBoolean(getString(R.string.setting_key_notify), false);
 					boolean play_sound = preferences.getBoolean(getString(R.string.setting_key_sound), false);
 					boolean vibrate = preferences.getBoolean(getString(R.string.setting_key_vibrate), false);
+
+					// Start builing notification
 					if (notify) {
 						Intent i = new Intent(this, MainActivity.class);
 						TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
 						stackBuilder.addNextIntent(i);
 						PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
+						// WEAR set sender-specific background
+						NotificationCompat.WearableExtender wearableExtender =
+								new NotificationCompat.WearableExtender();
+						if (notification.token.equals("weechat")) {
+							wearableExtender
+									.setBackground(BitmapFactory.decodeResource(getResources(), R.drawable.weechat))
+									.setHintShowBackgroundOnly(true);
+						}
+
+						// General Notification Settings
 						NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
 								.setSmallIcon(android.R.drawable.sym_action_email)
 								.setContentTitle(notification.title)
 								.setContentText(notification.message)
 								.setAutoCancel(true)
 								.setContentIntent(pendingIntent)
-								.setNumber(5);
+								.setNumber(5)
+								.extend(wearableExtender);
 
+						// Add Action if Message contains URL
 						if(!notification.url.isEmpty()){
 							Uri page = Uri.parse(notification.url);
 							Intent open_url = new Intent(Intent.ACTION_VIEW, page);
 							PendingIntent pi = PendingIntent.getActivity(this,0,open_url,0);
+
 							mBuilder.addAction(R.drawable.ic_action_web_site ,getString(R.string.open_url), pi);
 						}
 
-						Notification noti = mBuilder.build();
+						Notification noti = null;
+						// WEAR add full text to second page if shortend
+						if(!notification.long_message.isEmpty()){
+							NotificationCompat.BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
+							secondPageStyle.bigText(notification.long_message);
+
+							Notification secondPage = new NotificationCompat.Builder(this)
+									.setStyle(secondPageStyle)
+									.build();
+
+							noti =
+									wearableExtender
+									.addPage(secondPage)
+									.extend(mBuilder)
+									.build();
+						} else {
+							noti = mBuilder.build();
+						}
 
 						if (play_sound) {
 							noti.defaults |= Notification.DEFAULT_SOUND;
@@ -101,9 +133,11 @@ public class GcmIntentService extends IntentService {
 						if (vibrate) {
 							noti.defaults |= Notification.DEFAULT_VIBRATE;
 						}
+
+						// Send Notification
 						NotificationManagerCompat mNotificationManager =
 								NotificationManagerCompat.from(this);
-						mNotificationManager.notify(notificationID, noti);
+						mNotificationManager.notify(NOTIFICATION_ID, noti);
 					}
 				} else {
 					Log.i("empty Message");
