6f5c516a1ae6030e87d446c347b93e59cd45bf8b
Felix <mail@nicidienase.de>
2015-04-26 16:30:11 +0200

    added custom viewBinder for CursorAdapter

--- pushclient/src/main/java/de/nicidienase/push/pushclient/NotificationCursorAdapter.java
+++ pushclient/src/main/java/de/nicidienase/push/pushclient/NotificationCursorAdapter.java
@@ -1,7 +1,12 @@
 package de.nicidienase.push.pushclient;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Date;
 
 /**
  * Created by felix on 08/10/14.
@@ -14,5 +19,22 @@ public class NotificationCursorAdapter extends SimpleCursorAdapter {
 				new String[] {"title","message","received"},
 				new int[] {R.id.title_view, R.id.messageText,R.id.dateText},
 				0);
+		this.setViewBinder(new ViewBinder() {
+			@Override
+			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+				switch (cursor.getColumnName(columnIndex)) {
+					case "title":
+					case "message":
+						((TextView) view).setText(cursor.getString(columnIndex));
+						return true;
+					case "recieved":
+						Date d = new Date(cursor.getLong(columnIndex));
+						((TextView) view).setText(d.toString());
+						return true;
+					default:
+						return false;
+				}
+			}
+		});
 	}
 }
